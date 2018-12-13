4a8b940d49b9403fc83237f89fbefe78c673c57a
Marcus Mills <marcusmills0926@gmail.com>
2018-12-12 21:59:36 -0500

    Updated API calls for heroku

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,19 +1,22 @@
 angular.module('listings', []).factory('Listings', function($http) {
+  var url = 'https://database-interface.herokuapp.com';
+  // var url = 'http://localhost:8080'
+
   var methods = {
     getFlowers: function() {
-      return $http.get('http://localhost:8080/api/flowers');
+      return $http.get(url+'/api/flowers');
     },
 
     create: function(listing) {
-      return $http.post('http://localhost:8080/api/listings', listing);
+      return $http.post(url+'/api/listings', listing);
     },
 
     getSightings: function(comname) {
-      return $http.get('http://localhost:8080/api/flowers/'+comname)
+      return $http.get(url+'/api/flowers/'+comname)
     },
 
     delete: function(id) {
-       return $http.delete('http://localhost:8080/api/flowers/'+id);
+       return $http.delete(url+'/api/flowers/'+id);
     },
 
     getImage: function(query) {
@@ -21,11 +24,11 @@ angular.module('listings', []).factory('Listings', function($http) {
     },
 
     getLocations: function() {
-      return $http.get('http://localhost:8080/api/locations');
+      return $http.get(url+'/api/locations');
     },
 
     addSighting: function(sighting) {
-      return $http.post('http://localhost:8080/api/sightings', sighting);
+      return $http.post(url+'/api/sightings', sighting);
     },
 
     updateFlower: function(flower) {
